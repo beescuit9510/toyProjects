3c4faa54e06e4151ab8314e0f769f1bb09c8dddc
beescuit <pengsoon20@gmail.com>
2021-09-11 23:59:04 +0900

    0911 commit

--- PycharmProjects/pythonProject/MyDatastructure/heap_sort.py
+++ PycharmProjects/pythonProject/MyDatastructure/heap_sort.py
@@ -1,34 +1,39 @@
 def down_heap(array :[], left:int, right:int):
     temp_left_node = array[left]
 
-
     parent = left
 
     while parent < (right+1)/2:
-        child_left = int(parent*2+1)
-        child_right = int(child_left+1)
+        child_left = parent*2+1
+        child_right = child_left+1
 
-        condition_1 = child_right <= right
-        condition_2 = array[child_left] < array[child_right]
+        # print("parent : ",parent," c left",child_left," c right:",child_right," right: ", right)
 
-        child = child_right if condition_1 and condition_2 else child_left
+        if child_right >= right:
+            break
 
-        if array[child] < temp_left_node:
-            break;
+        condition_1 = array[child_left] < array[child_right]
+
+        child = child_right if condition_1 else child_left
+
+        if array[child] <= temp_left_node:
+            break
 
         array[parent] = array[child]
 
         parent = child
 
+    array[parent] = temp_left_node
 
-def heap_sort(array :[], length:int):
+
+def heap_sort(array:[], length:int):
 
     idx = int((length-1)/2)
-    while idx > 0:
+    while idx >= 0:
         down_heap(array, idx, length)
         idx-=1
 
-    idx = length -1
+    idx = length - 1
     while idx > 0:
         array[0], array[idx] = array[idx], array[0]
         down_heap(array, 0, idx-1)
